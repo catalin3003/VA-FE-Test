8ed0ed1c336723a70d83739b896b674cf2ed121e
Catalin Magirescu <cat@catcodes.com>
2024-04-12 09:59:45 +0100

    Refactor loading component and add tests

--- cypress/component/loading.cy.tsx
+++ cypress/component/loading.cy.tsx
@@ -1,9 +1,16 @@
 import Loading from "@/app/results/loading";
 
-
 describe('Loading Component', () => {
-  it('renders correctly', () => {
+  it('renders the loading animation', () => {
     cy.mount(<Loading />);
-    cy.get('.fullScreenLoader').should('exist');
+
+    const loading = cy.get('[data-testid="fullScreenLoader"]');
+    expect(loading).to.exist;
+
+    const loadingBar = cy.get('[data-testid="loadingBar"]');
+    expect(loadingBar).to.exist;
+
+    const bgImages = cy.get('[data-testid="backgroundImages"]');
+    expect(bgImages).to.exist;
   });
 });
--- src/app/results/loading.tsx
+++ src/app/results/loading.tsx
@@ -3,9 +3,9 @@ import styles from './loading.module.css';
 
 export default function Loading() {
     return (
-        <div className={styles.fullScreenLoader}>
-            <div className={styles.loadingBar}></div>
-            <div className={styles.backgroundImages}>
+        <div className={styles.fullScreenLoader} data-testid="fullScreenLoader">
+            <div className={styles.loadingBar} data-testid="loadingBar"></div>
+            <div className={styles.backgroundImages} data-testid="backgroundImages">
                 <div className={styles.backgroundImage}></div>
                 <div className={styles.backgroundImage}></div>
                 <div className={styles.backgroundImage}></div>
