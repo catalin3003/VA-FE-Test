e5d78c911a4040bab6493001769461a3ce5bc9bc
Catalin Magirescu <cat@catcodes.com>
2024-04-12 07:39:53 +0100

    Add loading component and styles for displaying loading animation

--- src/app/results/loading.tsx
+++ src/app/results/loading.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import styles from './loading.module.css';
+
 export default function Loading() {
-    return <p>Loading&hellip;</p>
+    return (
+        <div className={styles.fullScreenLoader}>
+            <div className={styles.loadingBar}></div>
+            <div className={styles.backgroundImages}>
+                <div className={styles.backgroundImage}></div>
+                <div className={styles.backgroundImage}></div>
+                <div className={styles.backgroundImage}></div>
+            </div>
+        </div>
+    );
 }
